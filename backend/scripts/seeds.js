9038e058e4c400bde2b05733c1a735af640a9be8
Carl Meyer <113263281+ukiyo-studio@users.noreply.github.com>
2023-10-29 22:02:51 +0000

    Add Schemas to Seed Script

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -3,9 +3,65 @@ const mongoose = require("mongoose");
 const connection = process.env.MONGODB_URI;
 mongoose.connect(connection);
 
-const User = mongoose.model("User");
-const Item = mongoose.model("Item");
-const Comment = mongoose.model("Comment");
+var UserSchema = new mongoose.Schema(
+    {
+      username: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: [true, "can't be blank"],
+        match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+        index: true
+      },
+      email: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: [true, "can't be blank"],
+        match: [/\S+@\S+\.\S+/, "is invalid"],
+        index: true
+      },
+      bio: String,
+      image: String,
+      role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+      },
+      favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
+      following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+      hash: String,
+      salt: String
+    },
+    { timestamps: true }
+  );
+
+  var ItemSchema = new mongoose.Schema(
+    {
+      slug: { type: String, lowercase: true, unique: true },
+      title: {type: String, required: [true, "can't be blank"]},
+      description: {type: String, required: [true, "can't be blank"]},
+      image: String,
+      favoritesCount: { type: Number, default: 0 },
+      comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+      tagList: [{ type: String }],
+      seller: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+    },
+    { timestamps: true }
+  );
+
+  var CommentSchema = new mongoose.Schema(
+    {
+      body: String,
+      seller: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      item: { type: mongoose.Schema.Types.ObjectId, ref: "Item" }
+    },
+    { timestamps: true }
+  );
+
+const User = mongoose.model("User", UserSchema);
+const Item = mongoose.model("Item", ItemSchema);
+const Comment = mongoose.model("Comment", CommentSchema);
 
 async function seedDatabase() {
     for (let i = 0; i < 100; i++) {
